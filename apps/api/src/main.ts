21a74471f07ea85be00bfef6aa45feaf0ca56ed6
Bartosz <barth.webdesign@gmail.com>
2024-10-21 22:56:42 +0200

    feat: init app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['error', 'warn', 'log'],
+    bufferLogs: true,
+  });
+
+  app.setGlobalPrefix('api/v1');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('FromPoland Currency Exchange')
+    .setDescription('API for FromPoland Currency Exchange')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('APP_PORT'));
 }
 bootstrap();
