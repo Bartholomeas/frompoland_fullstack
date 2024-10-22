b1a992f025df11091a7b2fa5166702ecedd9e659
Bartosz <barth.webdesign@gmail.com>
2024-10-22 12:20:33 +0200

    feat: init tailwind, shadcn and next structure

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,8 +1,17 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    '@repo/eslint-config/next.js',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:import/typescript',
+    'plugin:tailwindcss/recommended',
+    'eslint-config-prettier',
+    'prettier',
+    'next/core-web-vitals',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
