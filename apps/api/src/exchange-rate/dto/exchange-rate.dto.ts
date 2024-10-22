12532a513d7c41fdf23cd8bd4835b3798d019a99
Bartosz <barth.webdesign@gmail.com>
2024-10-22 10:29:54 +0200

    feat: add swagger docs to exchange rate endpoint

--- apps/api/src/exchange-rate/dto/exchange-rate.dto.ts
+++ apps/api/src/exchange-rate/dto/exchange-rate.dto.ts
@@ -1,12 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { Currency } from 'src/common/enums/currency.enum';
 
 export class ExchangeRateDto {
+  @ApiProperty({
+    default: Currency.EUR,
+    enum: Currency,
+  })
   @IsString()
   @IsOptional()
   @IsEnum(Currency)
   from: Currency = Currency.EUR;
 
+  @ApiProperty({
+    default: Currency.PLN,
+    enum: Currency,
+  })
   @IsString()
   @IsOptional()
   @IsEnum(Currency)
