89bf8d0901cc2355d73bd6b9189ec857863fe1d5
Bartosz <barth.webdesign@gmail.com>
2024-10-22 10:19:58 +0200

    feat: exchange rate dto simulation

--- apps/api/src/exchange-rate/exchange-rate.controller.ts
+++ apps/api/src/exchange-rate/exchange-rate.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  HttpException,
+  InternalServerErrorException,
+  Query,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-import { HttpCacheInterceptor } from 'src/common/interceptors/http-cache.interceptor';
 import { ExchangeRateService } from './exchange-rate.service';
+import { ExchangeRateDto } from './dto/exchange-rate.dto';
 
 @ApiTags('Exchange Rate')
 @Controller('exchange-rate')
-@UseInterceptors(HttpCacheInterceptor)
 export class ExchangeRateController {
   constructor(private readonly exchangeRateService: ExchangeRateService) {}
 
   @Get()
-  async getExchangeRate() {
+  @ApiOperation({ summary: 'Get exchange rate' })
+  async getExchangeRate(@Query() exchangeRateDto: ExchangeRateDto) {
     try {
-      return this.exchangeRateService.getExchangeRate();
-    } catch (err) {}
+      return await this.exchangeRateService.getExchangeRate(exchangeRateDto);
+    } catch (err) {
+      if (err instanceof HttpException) throw err;
+      throw new InternalServerErrorException();
+    }
   }
 }
